a11232d2b3786e73a3df35d19ac46fd9c7965c8a
Asande <22434600@dut4life.ac.za>
2026-03-22 21:03:16 +0200

    Fix email threading - build message inside app context

--- utils/email.py
+++ utils/email.py
@@ -1,17 +1,16 @@
 import threading
 from flask_mail import Message
 
-def send_async_email(app, msg):
+def send_async_email(app, subject, recipient, body):
     with app.app_context():
         from app import mail
+        msg = Message(subject=subject, recipients=[recipient], body=body)
         mail.send(msg)
 
-def send_email(subject, recipient, body, html_body=None):
+def send_email(subject, recipient, body):
     from app import app
-    with app.app_context():
-        from app import mail
-        msg = Message(subject=subject, recipients=[recipient], body=body, html=html_body)
-    thread = threading.Thread(target=send_async_email, args=(app, msg))
+    thread = threading.Thread(target=send_async_email, args=(app, subject, recipient, body))
+    thread.daemon = True
     thread.start()
 
 def send_booking_confirmation(user_email, user_name, facility_name, start_time, end_time, booking_id):
